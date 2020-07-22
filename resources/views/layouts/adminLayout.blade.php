410ec10da0e7c51c121fef30ecced63fb3acd846
Cesa Rahman <cesakotob13@gmail.com>
2020-07-22 21:43:28 +0700

    test clome local repo

--- resources/views/layouts/adminLayout.blade.php
+++ resources/views/layouts/adminLayout.blade.php
@@ -35,7 +35,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Admin UB</div>
+        <div class="sidebar-brand-text mx-3">Admin Prodi TI</div>
       </a>
 
       <!-- Divider -->
@@ -380,7 +380,7 @@
 
   <!-- Page level plugins -->
   <script src="vendor/chart.js/Chart.min.js"></script>
-  
+
   <script src="vendor/datatables/jquery.dataTables.min.js"></script>
   <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
@@ -389,7 +389,7 @@
   <script src="js/demo/chart-pie-demo.js"></script>
   <script src="js/demo/datatables-demo.js"></script>
 
-  
+
 
 </body>
 
